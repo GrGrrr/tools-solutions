cc600673b4cd5fd789e551b5a0e5936607c3bbd7
ogrishchenko <melkij.slasher@gmail.com>
2024-11-07 03:01:41 +0300

    fix styles

--- components/CaseSection/components/VerticalTab.jsx
+++ components/CaseSection/components/VerticalTab.jsx
@@ -3,10 +3,10 @@ export const VerticalTab = ({item, onClick, activeTab, index}) => {
 
     return (
         <div
-            className="flex items-center h-[80px] mb-[10px]"
+            className="flex items-center justify-center h-[80px] mb-[10px]"
             onClick={handleClick}>
             <div className={`border-[1px] border-base-strong w-full mr-[40px] ${activeTab === index ? 'visible' : 'invisible'}`} />
-            <div>
+            <div className="flex justify-center min-w-[182px]">
                 {item.logo}
             </div>
         </div>
